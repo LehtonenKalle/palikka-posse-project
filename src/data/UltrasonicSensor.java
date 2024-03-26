ae3cfd9b51284a3b431f90870e73b16af356f26e
LehtonenKalle <kalle23013@student.hamk.fi>
2024-03-26 11:04:08 +0200

    update ultrasonic

--- src/data/UltrasonicSensor.java
+++ src/data/UltrasonicSensor.java
@@ -1,5 +1,35 @@
 package data;
 
-public class UltrasonicSensor {
+import lejos.hardware.Button;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
+
+public class UltrasonicSensor implements Runnable {
+	
+	private DataExchange DEObj = new DataExchange();
+	private static EV3UltrasonicSensor us = new EV3UltrasonicSensor(SensorPort.S3);
+	
+	public UltrasonicSensor(DataExchange DE) {
+		DEObj = DE;
+	}
+	
+	public void run() {
+		if (getDistance() < 7) {
+			DEObj.setObstacleDetected(true);
+		}
+	}
+	
+	public int getDistance() {
+        final SampleProvider sp = us.getDistanceMode();
+        int distanceValue = 0;
+        
+        float [] sample = new float[sp.sampleSize()];
+        sp.fetchSample(sample, 0);
+        distanceValue = (int)(sample[0]*100);
+        
+        return distanceValue;
+	}
 }

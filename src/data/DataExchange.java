aaaba1ada1e96474e9ff1ae95e0340210cf47092
NikoLaaks <niko23010@student.hamk.fi>
2024-03-26 10:48:49 +0200

    Create DataExchange.java

---
+++ src/data/DataExchange.java
@@ -0,0 +1,17 @@
+package data;
+
+public class DataExchange {
+	private boolean	obstacleDetected = false;
+	public DataExchange() {
+		
+	}
+		
+		public void setObstacleDetected(boolean status) {
+			obstacleDetected = status;
+		}
+		public boolean getObstacleDetected() {
+			return obstacleDetected;
+		}
+	
+	
+}

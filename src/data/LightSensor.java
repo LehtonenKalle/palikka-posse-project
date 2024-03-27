d54bcf7b53a8fd509a83f7dc0e95963465893bf6
arttu22012 <142879369+arttu22012@users.noreply.github.com>
2024-03-27 11:06:50 +0200

    lightsensor update

--- src/data/LightSensor.java
+++ src/data/LightSensor.java
@@ -1,5 +1,33 @@
 package data;
 
-public class LightSensor {
- 
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.SampleProvider;
+
+public class LightSensor implements Runnable {
+
+    private static EV3ColorSensor cs = new EV3ColorSensor(SensorPort.S3);
+
+    public void run() {
+        while (true) {
+            double colorValue = getColor();
+            if (colorValue < 0.1) {
+            	colorValue = 
+                // Send data to motors
+            }
+            else {
+            	
+                // Send data to motors
+            }
+        }
+    }
+
+    public double getColor() {
+        final SampleProvider colorSP = cs.getRedMode();
+        cs.setFloodlight(true);
+        float[] colorSample = new float[colorSP.sampleSize()];
+        colorSP.fetchSample(colorSample, 0);
+        double colorValue = colorSample[0];
+        return colorValue;
+    }
 }

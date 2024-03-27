8f45c6f08410d3f5632fddc510e1e16f5d09603d
joni23008 <joni.mitronen@student.hamk.fi>
2024-03-27 10:00:29 +0200

    Update to Motors.
    After this i start my own branch.

--- src/data/Motors.java
+++ src/data/Motors.java
@@ -1,6 +1,7 @@
 package data;
 
 import lejos.hardware.Button;
+import lejos.hardware.Sound;
 import lejos.hardware.motor.UnregulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.utility.Delay;
@@ -9,28 +10,13 @@ public class Motors implements Runnable{
 	//------------------------------------------------------------------------------
 	// Outside the Thread
 	//------------------------------------------------------------------------------
-//Huom. Sisälinjaan!
-//	Huom. 0.22 kokonaan valkonen
-//	Huom. 0.04 kokonaan musta
-//	Huom. 0.1 mustan ja valkosen välissä
-// 	Huom. A = Vasen JA B = Oikea	
-//While looppi, jossa tarkistetaan
-//	DataExchange.isLineDetected()
-//	SEKÄ
-//	DataExchange.isObjectDetected()
-//		1. jos ei LINJAA ole niin tehdään korjausliikkeitä TRUE
-//		2. jos LINJA kiikarissa jatketaan eteenpäin FALSE
-	
-	//Moottorit pysähtyy
-    //motorA.stop();
-    //motorB.stop();
+	// Alustetaan nopeus atribuutti.
 	private int speed;
-		
-
+	
 	// Luo kaksi moottori objektia.
-	// Vasen.
+	// A Vasen.
     UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
-    // Oikea.
+    // B Oikea.
     UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.B);
     
 	public int getSpeed() {
@@ -46,96 +32,72 @@ public class Motors implements Runnable{
 	    else if (speed < -100) {
 	        this.speed = -100;
 	    }
-	    // Jos nopeus on hyväksytyissä rajoissa, asetetaan se.
+	    // Jos nopeus on hyväksytyissä rajoissa, asetetaan se ja jatketaan eteenpäin.
 	    else {
 	        this.speed = speed;
-	    }
-	    
-	    // TRUE kun linja on näkyvissä.Korjausliikkeitä.
-	    if (DataExchange.isLineDetected()) {
-	        //KÄÄNNÖS 1.
-	         
-	        	motorA.setPower(50);
-	        	motorB.setPower(25);        
-	        	// Tietoa color sensorin arvoista.
-	        	//System.out.println("color value: " + colorValue);
-	        	//Tietoa ultra sensorin arvoista
-	        	//System.out.println("distance: " + distanceValue);
-	        
-	    }
-        //KÄÄNNÖS 2.
-    	else {
-    		motorA.setPower(25);
-        	motorB.setPower(50);   
-        	// Tietoa color sensorin arvoista.
-        	//System.out.println("color value: " + colorValue);
-        	//Tietoa ultra sensorin arvoista
-        	//System.out.println("distance: " + distanceValue);
-    	}
-   
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-	    // TRUE kun linja on näkyvissä.Täysillä eteenpäin.
-	    while (DataExchange.isLineDetected()) {
-	    	motorA.setPower(this.speed);
-	        motorB.setPower(this.speed);
-	    }
-	    
-	    // FALSE kun este ei ole näkyvissä.Täysillä eteenpäin.
-	    while (!DataExchange.isObjectDetected()) {
 	        motorA.setPower(this.speed);
-	        motorB.setPower(this.speed);
-	    }
-	    
-	    // TRUE kun este on näkyvissä.Korjausliikkeitä.
-	    if (DataExchange.isObjectDetected()) {
-//	    	VANHA-----------------------------------------
-//		    if (distanceValue < 7) {
-//	        	// take a turn
-//	        	motorA.setPower(50);
-//	        	motorB.setPower(-50);
-//	        	Delay.msDelay(500);
-//	        	do {
-//	            	// go straight again
-//	            	motorA.setPower(30);
-//	            	motorB.setPower(50);
-//	        	} while(colorValue > 0.1);
-//
-//	        	motorA.setPower(-50);
-//	        	motorB.setPower(50);
-//	        	Delay.msDelay(300);
-//	        }
-//	    	VANHA-----------------------------------------
-	    }
-     
-	    // Pysähtyminen.
-	    if (Button.getButtons()!= 0) {
-//	    	VANHA-----------------------------------------
-//		    motorA.close();
-//          motorB.close();
-//          break;
-//	    	VANHA-----------------------------------------
-	    }
+        	motorB.setPower(this.speed);
+        	do {
+        	    // Tarkistetaan onko nappia painettu käynnistyksen jälkeen.
+        	    if (Button.getButtons() != 0) {
+        	        // Moottorit pysäytetään.
+        	        motorA.stop();
+        	        motorB.stop();
+        	        // Moottoreiden resurssit vapautetaan.
+        		    motorA.close();
+        		    motorB.close();
+        		    // Loop loppuu.
+        		    break;
+        	    } else {
+        	    	// LINJA TRUE, Linja näkyvissä. KÄÄNNÖS 1. Rajat asetettu Lightsensor luokassa.
+        		    if (DataExchange.getLineDetected()) {
+        		    	// A Vasen.
+        		        // B Oikea.
+        		        	motorA.setPower(50);
+        		        	motorB.setPower(25);        
+        		        	// Tietoa color sensorin arvoista.
+        		        	//System.out.println("color value: " + colorValue);
+        		        	//Tietoa ultra sensorin arvoista
+        		        	//System.out.println("distance: " + distanceValue);
+        		    }
+        		    // LINJA FALSE, Linja näkyvissä. KÄÄNNÖS 2. Rajat asetettu Lightsensor luokassa.
+        		    if (!DataExchange.getLineDetected()) {
+        		    	// A Vasen.
+        		        // B Oikea.
+        		        	motorA.setPower(25);
+        		        	motorB.setPower(50);        
+        		        	// Tietoa color sensorin arvoista.
+        		        	//System.out.println("color value: " + colorValue);
+        		        	//Tietoa ultra sensorin arvoista
+        		        	//System.out.println("distance: " + distanceValue);
+        		    }
+        		    // ESTE TRUE, kun este on näkyvissä.Korjausliikkeitä.
+        		    if (DataExchange.getObstacleDetected()) {
+        		    	// A Vasen.
+        		        // B Oikea.
+        		        	// Nopea suunnan muutos pois päin esteestä. 90 astetta.
+        		        	motorA.setPower(50);
+        		        	motorB.setPower(-50);
+        		        	Delay.msDelay(1000);
+        		        	// Jatketaan suoraan hetki.
+        		            motorA.setPower(50);
+        		            motorB.setPower(50);
+        		            Delay.msDelay(1000);
+        		        	// Nopea suunnan muutos takaisin viivalle. 90 astetta.
+        		        	motorA.setPower(-50);
+        		        	motorB.setPower(50);
+        		        	Delay.msDelay(1000);
+        		    }
+        	    }
+        	} while (Button.getButtons() == 0);//Loop jatkuu niin kauan kunnes nappia painetaan.
+	    }  
     }//End of setter speed.
-	    
-	    
-	    
-	    
-	
+	   
 	@Override
 	public void run() {
 		//------------------------------------------------------------------------------
 		// Inside the Thread
-		//------------------------------------------------------------------------------b
+		//------------------------------------------------------------------------------
 		
 	}
-	
-	
-	
-
-}
+}//End of class.

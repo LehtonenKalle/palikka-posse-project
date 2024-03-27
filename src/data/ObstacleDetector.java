c9366f32aa56864f6872ddfa911475e72066c95b
LehtonenKalle <kalle23013@student.hamk.fi>
2024-03-27 16:44:27 +0200

    comments to ObstacleDetector

--- src/data/ObstacleDetector.java
+++ src/data/ObstacleDetector.java
@@ -8,31 +8,43 @@ import lejos.utility.Delay;
 
 
 public class ObstacleDetector implements Runnable {
+	// Initializing DataExchange and EV3UltrasonicSensor
 	private DataExchange DEObj = new DataExchange();
 	private static EV3UltrasonicSensor us = new EV3UltrasonicSensor(SensorPort.S4);
 	
 	public ObstacleDetector(DataExchange DE) {
+		// Setting up DataExchange object that is sent to this constructor as an argument
 		DEObj = DE;
 	}
 	
 	public void run() {
+		// Looping 
 		while (true) {
 			//System.out.println(getDistance());
+			// Using getDistance()-method to get the distance value
+			// If distance to the object is under 7cm, send the information to DE
 			if (getDistance() < 7) {
 				DEObj.setObstacleDetected(true);
-			} else {
+			}
+			// If distance to the object is over 7cm, send the information to DE
+			else {
 				DEObj.setObstacleDetected(false);
 			}
 		}
 
 	}
 	
+	
 	public int getDistance() {
+		// Initializing sp
         final SampleProvider sp = us.getDistanceMode();
         int distanceValue = 0;
         
+        // Making an array that is the size of the sample
         float [] sample = new float[sp.sampleSize()];
+        // Fetching the sample to the array
         sp.fetchSample(sample, 0);
+        // Setting the DistanceValue as the value found from the 0th index and multiplying it by 100 (to make the value as cm)
         distanceValue = (int)(sample[0]*100);
         
         return distanceValue;

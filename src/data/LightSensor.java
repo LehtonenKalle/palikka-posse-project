efe63fb686fb044794a3bd8e88f3c24ea766dcb4
arttu22012 <142879369+arttu22012@users.noreply.github.com>
2024-04-02 11:37:29 +0300

    commetteja

--- src/data/LightSensor.java
+++ src/data/LightSensor.java
@@ -5,6 +5,7 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
 
 public class LightSensor implements Runnable {
+	// Initializing DataExchange and EV3LightSensor
 	private DataExchange DEObj = new DataExchange();
     private static EV3ColorSensor cs = new EV3ColorSensor(SensorPort.S3);
 
@@ -14,27 +15,32 @@ public class LightSensor implements Runnable {
     
     public void run() {
         while (true) {
+        	// Using getColor()-method to get the light value
             double colorValue = getColor();
             System.out.println("color value:" + colorValue);            //System.out.println(colorValue);
             if (colorValue < 0.1) {
-            	//colorValue = 
-                // Send data to motors
+
+                // Send data to dataExchange
             	DEObj.setLineDetected(true);
             	
             }
             // black = 0.1
             else if (colorValue > 0.1){
-                // Send data to motors
+                // Send data to dataExchange
             	DEObj.setLineDetected(false);
             }
         }
     }
 
     public double getColor() {
+    	// Initializing sample provider
         final SampleProvider colorSP = cs.getRedMode();
         cs.setFloodlight(true);
+        // array 
         float[] colorSample = new float[colorSP.sampleSize()];
+        // Fetching the sample to the array
         colorSP.fetchSample(colorSample, 0);
+        // Setting the DistanceValue as the value found from the 0th index
         double colorValue = colorSample[0];
         return colorValue;
     }

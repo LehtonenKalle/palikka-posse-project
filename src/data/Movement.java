b2e98dbf6a4fecb810bd30e778544dac378f42c4
LehtonenKalle <kalle23013@student.hamk.fi>
2024-04-05 23:05:28 +0300

    commit to revert changes

--- src/data/Movement.java
+++ src/data/Movement.java
@@ -1,59 +1,32 @@
 package data;
 
-/**
- * This class is used for the reverse functionality in Motors-class.
- * Motors.java calls this class for each unique movement
- */
 public class Movement {
 	private int powerA;
     private int powerB;
     private int timestamp;
 
-    /**
-     * @param powerA current power value of the motorA
-     * @param powerB current power value of the motorB
-     * @param timestamp integer value of the current time
-     */
     public Movement(int powerA, int powerB, int timestamp) {
         this.powerA = powerA;
         this.powerB = powerB;
         this.timestamp = timestamp;
     }
-    /**
-     * @param powerA current power value of the motorA
-     * @param powerB current power value of the motorB
-     */
     public Movement(int powerA, int powerB) {
     	this.powerA = powerA;
         this.powerB = powerB;
     }
     
-    /**
-     * sets the timestamp value 
-     * @param integer value of the current time in ms
-     */
     public void setTimestamp(int timestamp) {
     	this.timestamp = timestamp;
     }
-    
-    
  // Getter methods for powerA, powerB, and timestamp
-    /**
-     * @return returns the current value of motorA
-     */
     public int getPowerA() {
         return powerA;
     }
-    /**
-     * @return returns the current value of motorB
-     */
+
     public int getPowerB() {
         return powerB;
     }
 
-    /**
-     * @return returns the current timestamp
-     */
     public int getTimestamp() {
         return timestamp;
     }

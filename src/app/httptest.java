33dd8d468aef1fa90027db509a5aaaba775c373d
NikoLaaks <niko23010@student.hamk.fi>
2024-04-24 10:39:21 +0300

    update

--- src/app/httptest.java
+++ src/app/httptest.java
@@ -16,6 +16,8 @@ import java.sql.Date;
 import java.sql.Time;
 import java.util.Calendar;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 //import javax.ws.rs.client.Client;
 //import javax.ws.rs.client.ClientBuilder;
@@ -44,7 +46,7 @@ public class httptest {
 		try {
 			url = new URL("http://192.168.1.11:8080/rest/lego/getvalues");
 			conn = (HttpURLConnection)url.openConnection();
-  			System.out.println(conn.toString()); //Tulostaa vain URLin
+  			System.out.println(conn.toString()); 
 			InputStream is=null;
 			try {
 				is=conn.getInputStream();
@@ -56,14 +58,48 @@ public class httptest {
 			}
 			isr = new InputStreamReader(is);
       		br=new BufferedReader(isr);
-			while ((s=br.readLine())!=null){
-				System.out.println(s);
-			}
+      		
+      		s=br.readLine();
+      		
+//			while ((s=br.readLine())!=null){
+//				System.out.println(s);
+//			}
 		}
   		catch(Exception e) {
   			e.printStackTrace();
             System.out.println("Some problem!");
   		}
+		
+		
+		// Remove curly braces and split by comma to get key-value pairs
+        String[] keyValuePairs = s.replaceAll("[{}\"]", "").split(",");
+        
+        // Create a map to store key-value pairs
+        Map<String, String> map = new HashMap<>();
+        
+        for (String pair : keyValuePairs) {
+            // Split each key-value pair by colon
+            String[] keyValue = pair.split(":");
+            
+            // Trim any leading or trailing whitespace
+            String key = keyValue[0].trim();
+            String value = keyValue[1].trim();
+            
+            // Remove any leading or trailing quotes
+            key = key.replaceAll("^\"|\"$", "");
+            value = value.replaceAll("^\"|\"$", "");
+            
+            // Put key-value pair into the map
+            map.put(key, value);
+        }
+        
+        // Example: Get value associated with "key2"
+        String colorvalue = map.get("colorvalue");
+        System.out.println("Value for colorvalue: " + colorvalue);
+    
+
+	
+		
         System.out.println("Press any key to FINISH");
         Button.waitForAnyPress();
 	}

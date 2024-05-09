9bc9accb35f2b095f0d8aeae521de85a21838b62
joni23008 <joni.mitronen@student.hamk.fi>
2024-05-09 13:45:27 +0300

    Documentation

--- src/data/DataSender.java
+++ src/data/DataSender.java
@@ -8,75 +8,90 @@ import java.nio.charset.StandardCharsets;
 
 import com.google.gson.Gson;
 
+/**
+ * The DataSender class sends data to a remote server via HTTP POST requests at regular intervals.
+ * It converts data from a DataToDatabase object to JSON format and sends it to the specified endpoint.
+ * Implements the Runnable interface to enable execution in a separate thread.
+ */
 public class DataSender implements Runnable {
-	private DataToDatabase DTD = new DataToDatabase();
-	private static final int sendIntervalMS = 1000; //1 second interval
-	
-	public DataSender(DataToDatabase dtd) {
-		DTD = dtd;
-	}
-	
-	public void run() {
-		try {
-            Thread.sleep(1000); // Wait for 1 seconds at start
+
+    /** The interval (in milliseconds) at which data is sent to the server. */
+    private static final int sendIntervalMS = 1000; // 1 second interval
+
+    /** The DataToDatabase object containing the data to be sent. */
+    private DataToDatabase DTD;
+
+    /**
+     * Constructs a DataSender object with the specified DataToDatabase object.
+     * @param dtd The DataToDatabase object containing the data to be sent.
+     */
+    public DataSender(DataToDatabase dtd) {
+        DTD = dtd;
+    }
+
+    /**
+     * Runs the data sending process in a separate thread.
+     * Continuously sends data to the remote server at regular intervals.
+     */
+    public void run() {
+        try {
+            Thread.sleep(1000); // Wait for 1 second at start
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-		
-		
-		while(true) {
-			
-		
-	        try {
-	            String jsonData = convertToJson(DTD);
-	            // Create URL and connection
-	            URL url = new URL("http://192.168.1.187:8080/rest/lego/sendstatistics");
-	            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	            conn.setRequestMethod("POST"); // Use POST method
-	            conn.setRequestProperty("Content-Type", "application/json");
-	            conn.setDoOutput(true);
 
-	            
-	            OutputStream os = conn.getOutputStream();
-	            OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
-	            osw.write(jsonData);
-	            osw.flush();
+        while (true) {
+            try {
+                // Convert DataToDatabase object to JSON format
+                String jsonData = convertToJson(DTD);
 
-	            // Check response code
-	            int responseCode = conn.getResponseCode();
-	            if (responseCode == HttpURLConnection.HTTP_OK) {
-	                System.out.println("Data sent successfully");
-	            } else {
-	                System.out.println("Failed to send data. Response code: " + responseCode);
-	            }
+                // Create URL connection
+                URL url = new URL("http://192.168.1.187:8080/rest/lego/sendstatistics");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST"); // Use POST method
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setDoOutput(true);
 
-	            // Close connections
-	            osw.close();
-	            os.close();
-	            conn.disconnect();
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	            System.out.println("Some problem!");
-	        }
-	        try {
-	            Thread.sleep(sendIntervalMS); // Adjust as needed
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
-	        }
-	    }
-	}
-	
-	
-	private static String convertToJson(DataToDatabase data) {
-	    Gson gson = new Gson();
-	    return gson.toJson(data);
-	}
-		
-		
-		
-		
-		
-		
-	}
-	
+                // Write JSON data to output stream
+                OutputStream os = conn.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
+                osw.write(jsonData);
+                osw.flush();
+
+                // Check response code
+                int responseCode = conn.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    System.out.println("Data sent successfully");
+                } else {
+                    System.out.println("Failed to send data. Response code: " + responseCode);
+                }
+
+                // Close connections
+                osw.close();
+                os.close();
+                conn.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("Some problem!");
+            }
+
+            try {
+                Thread.sleep(sendIntervalMS); // Adjust as needed
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Converts a DataToDatabase object to JSON format using Gson library.
+     * @param data The DataToDatabase object to be converted.
+     * @return The JSON representation of the DataToDatabase object.
+     */
+    private static String convertToJson(DataToDatabase data) {
+        Gson gson = new Gson();
+        return gson.toJson(data);
+    }
+}
+
 
--- src/data/DataToDatabase.java
+++ src/data/DataToDatabase.java
@@ -8,41 +8,64 @@ import lejos.hardware.Battery;
  */
 
 public class DataToDatabase {
-	private volatile int on_line_time;
-	private volatile double battery_voltage;
-	private volatile int obstacle_distance;
-	
-	public DataToDatabase() {
-	}
-
-	public int getOn_line_time() {
-		return on_line_time;
-	}
-
-	public void setOn_line_time(int line_time) {
-		this.on_line_time = line_time;
-	}
-
-	public double getBattery_voltage() {
-		battery_voltage = Battery.getBatteryCurrent();
-		return battery_voltage;
-	}
-
-	public void setBattery_voltage(double battery_voltage) {
-		this.battery_voltage = battery_voltage;
-	}
-
-	public int getObstacle_distance() {
-		return obstacle_distance;
-	}
-
-	public void setObstacle_distance(int obstacle_distance) {
-		this.obstacle_distance = obstacle_distance;
-	}
-	
-	
-
-
+	/** The amount of time the device has been online (in seconds). */
+    private volatile int on_line_time;
+    
+    /** The current battery voltage of the device. */
+    private volatile double battery_voltage;
+    
+    /** The distance to the nearest obstacle (in some unit of measurement). */
+    private volatile int obstacle_distance;
+    
+    /**
+     * Constructs a new DataToDatabase object with default values.
+     */
+    public DataToDatabase() {
+    }
+    /**
+     * Retrieves the online time of the device.
+     * @return The online time in seconds.
+     */
+    public int getOn_line_time() {
+        return on_line_time;
+    }
+    /**
+     * Sets the online time of the device.
+     * @param line_time The online time to set in seconds.
+     */
+    public void setOn_line_time(int line_time) {
+        this.on_line_time = line_time;
+    }
+    /**
+     * Retrieves the battery voltage of the device.
+     * @return The battery voltage in volts.
+     */
+    public double getBattery_voltage() {
+        // Retrieve the current battery voltage
+        battery_voltage = Battery.getBatteryCurrent();
+        return battery_voltage;
+    }
+    /**
+     * Sets the battery voltage of the device.
+     * @param battery_voltage The battery voltage to set in volts.
+     */
+    public void setBattery_voltage(double battery_voltage) {
+        this.battery_voltage = battery_voltage;
+    }
+    /**
+     * Retrieves the obstacle distance from the device.
+     * @return The obstacle distance in some unit of measurement.
+     */
+    public int getObstacle_distance() {
+        return obstacle_distance;
+    }
+    /**
+     * Sets the obstacle distance for the device.
+     * @param obstacle_distance The obstacle distance to set in some unit of measurement.
+     */
+    public void setObstacle_distance(int obstacle_distance) {
+        this.obstacle_distance = obstacle_distance;
+    }
 	
 	
 }
